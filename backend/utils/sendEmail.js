e4b1141530e51b37a3dc814d701cf146dcf287a5
Himanshu <himanshukr.singh62@gmail.com>
2026-05-18 12:31:28 +0530

    updated project

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async ({ email, subject, message }) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL__USER_PASS, // App Password mapping
+      },
+    });
+
+    const mailOptions = {
+      from: `"ShopNest Support" <${process.env.GMAIL_USER_PASS}>`,
+      to: email,
+      subject: subject,
+      html: message,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log(`Email successfully sent to ${email}`);
+  } catch (error) {
+    console.error(`Failed to send email to ${email}: ${error.message}`);
+  }
+};
+
+module.exports = sendEmail;
